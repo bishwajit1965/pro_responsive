fd520d55f0a2e95b411ab5060189cece37a42a66
bishwajit1965 <paul.bishwajit09@gmail.com>
2019-01-13 23:06:27 +0600

    Added all files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Bootstrap Carusel</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <div class="container">
+            <h2>Responsive web page design</h2>
+            <div class="row">
+                <div class="col-sm-3">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae laudantium aperiam animi totam quia, magnam, quas, sunt numquam fugit magni optio amet deserunt necessitatibus? Hic minus esse voluptas similique ipsa.</p>
+                </div>
+                <div class="col-sm-9">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit officiis consequatur nobis quos commodi esse, maiores libero quidem ducimus dignissimos voluptate sed. Culpa enim nemo vero saepe tempora libero incidunt!</p>
+                </div>
+            </div>
+        </div>
+        <!-- Optional JavaScript -->
+        <script src="js/jquery-3.3.1.slim.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
