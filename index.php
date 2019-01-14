d7145b97b744e6ff6ccc2e9573c9965dcde2e7a2
bishwajit1965 <paul.bishwajit09@gmail.com>
2019-01-14 21:17:54 +0600

    "Changed"

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
             <h2>Responsive web page design</h2>
             <div class="row">
                 <div class="col-sm-3 left-sidebar">
-                    <p>Hellow world!</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae laudantium aperiam animi totam quia, magnam, quas, sunt numquam fugit magni optio amet deserunt necessitatibus? Hic minus esse voluptas similique ipsa.</p>
                 </div>
                 <div class="col-sm-9 right-sidebar">
